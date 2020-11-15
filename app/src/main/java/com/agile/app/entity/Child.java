be0de7a26bce5ba8881faa41ac30cb815b58b996
Gergo1212 <gergo.meszaros.codecool@gmail.com>
2020-11-15 16:27:39 +0100

    added basic field names and annotations to entities

--- app/src/main/java/com/agile/app/entity/Child.java
+++ app/src/main/java/com/agile/app/entity/Child.java
@@ -1,4 +1,25 @@
 package com.agile.app.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Child {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
 }
--- app/src/main/java/com/agile/app/entity/Parent.java
+++ app/src/main/java/com/agile/app/entity/Parent.java
@@ -1,4 +1,26 @@
 package com.agile.app.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Parent {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+
 }
